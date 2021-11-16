956c72c86e343757b3b02145c5e54aa6f3e88fb5
Aleksander Ściborek <aleksandersciborek@gmail.com>
2021-11-16 23:43:45 +0100

    Change double type to decimal for the price field

--- scripts/modifications/1_insert_books.js
+++ scripts/modifications/1_insert_books.js
@@ -3,7 +3,7 @@ db.books.insertMany([
     "_id": "978-0321349606",
     "title": "Java Concurrency in Practice",
     "total_pages": 403,
-    "price": 27.10,
+    "price": NumberDecimal("27.10"),
     "authors": ["Brian Goetz", "Tim Peierls", "Joshua Bloch", "Joseph Bowbeer", "David Holmes", "Doug Lea"],
     "keywords": ["Java", "JVM", "Concurrency"]
   },
@@ -11,7 +11,7 @@ db.books.insertMany([
     "_id": "978-0134685991",
     "title": "Effective Java 3rd Edition",
     "total_pages": 416,
-    "price": 53.99,
+    "price": NumberDecimal("53.99"),
     "authors": ["Joshua Bloch"],
     "keywords": ["Java", "JVM"]
   },
@@ -19,7 +19,7 @@ db.books.insertMany([
     "_id": "978-1942878957",
     "title": "Kotlin Coroutines by Tutorials (Second Edition): Mastering Coroutines in Kotlin and Android",
     "total_pages": 352,
-    "price": 27.10,
+    "price": NumberDecimal("27.10"),
     "authors": ["Filip Babić", "Nishant Srivastava"],
     "keywords": ["Kotlin", "JVM", "Android", "Concurrency"]
   },
@@ -27,7 +27,7 @@ db.books.insertMany([
     "_id": "978-1617293290",
     "title": "Kotlin in Action",
     "total_pages": 352,
-    "price": 21.00,
+    "price": NumberDecimal("21.00"),
     "authors": ["Dmitry Jemerov", "Svetlana Isakova"],
     "keywords": ["Kotlin", "JVM", "Android"]
   },
@@ -35,7 +35,7 @@ db.books.insertMany([
     "_id": "978-0134494166",
     "title": "Clean Architecture: A Craftsman's Guide to Software Structure and Design",
     "total_pages": 432,
-    "price": 20.00,
+    "price": NumberDecimal("20.00"),
     "authors": ["Robert C. Martin"],
     "keywords": ["Software Architecture", "Clean Code"]
   },
@@ -43,7 +43,7 @@ db.books.insertMany([
     "_id": "978-1449373320",
     "title": "Designing Data-Intensive Applications: The Big Ideas Behind Reliable, Scalable, and Maintainable Systems",
     "total_pages": 432,
-    "price": 20.00,
+    "price": NumberDecimal("20.00"),
     "authors": ["Martin Kleppmann"],
     "keywords": ["Software Architecture", "Distributed Systems", "Databases"]
   },
@@ -51,7 +51,7 @@ db.books.insertMany([
     "_id": "978-1492043089",
     "title": "Kafka: The Definitive Guide: Real-Time Data and Stream Processing at Scale 2nd Edition",
     "total_pages": 488,
-    "price": 43.00,
+    "price": NumberDecimal("43.00"),
     "authors": ["Gwen Shapira", "Todd Palino", "Rajini Sivaram", "Krit Petty"],
     "keywords": ["Distributed Systems", "Event-Driven Architecture"]
   }
